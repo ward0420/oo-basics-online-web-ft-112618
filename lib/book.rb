884251d9d58700fd6392c3814913e804ad594f82
Don Ward <10is0420@gmail.com>
2018-12-20 04:02:43 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
-
+class Book
+  
+  def initialize (title)
+    
+    @title = title
+    
+  end
+  
+  def title= (title)
+    
+    @title
+    
+  end
+  
+  def title
+    
+    @title
+    
+  end
+  
+  Book.new("And Then There Were None")
+  
+end
 
