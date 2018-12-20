7869d742022c07606f8388ccc215af56fbce4c0b
Don Ward <10is0420@gmail.com>
2018-12-20 04:14:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,59 +1,39 @@
 class Book
-  
-  def initialize (title)
-    
+  def initialize(title)
     @title = title
-    
   end
-  
+ 
   def title
-    
     @title
-    
   end
-  
-  def author =(author)
-    
-    @author =  author
-    
+ 
+  def author=(author)
+    @author = author
   end
-  
+ 
   def author
-    
     @author
-    
   end
-  
-  def page_count =(num)
-    
+ 
+  def page_count=(num)
     @page_count = num
-    
   end
-  
+ 
   def page_count
-    
     @page_count
-    
   end
-  
-  def genre = (genre)
-    
+ 
+  def genre=(genre)
     @genre = genre
-    
   end
-  
+ 
   def genre
-    
     @genre
-    
   end
-  
+ 
   def turn_page
-    
     puts "Flipping the page...wow, you read fast!"
-    
   end
-  
-  
+ 
 end
 
