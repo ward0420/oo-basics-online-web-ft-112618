788302137738f56be5a9b0c10d72d0a1572d519d
Don Ward <10is0420@gmail.com>
2018-12-20 04:09:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -18,7 +18,46 @@ class Book
     
   end
   
-  Book.new("And Then There Were None")
+  def author =(author)
+    
+    @author =  author
+    
+  end
+  
+  def author
+    
+    @author
+    
+  end
+  
+  def page_count =(num)
+    
+    @page_count = num
+    
+  end
+  
+  def page_count
+    
+    @page_count
+    
+  end
+  
+  def genre = (genre)
+    
+    @genre = genre
+    
+  end
+  
+  def genre
+    
+    @genre
+    
+  end
+  
+  def turn_page
+    puts ""
+  end
+  
   
 end
 
